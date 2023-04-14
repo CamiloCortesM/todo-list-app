004ddead516e4bff0e6974c20ecb973681cbf087
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-04-13 21:40:31 -0500

    validations and optimizations

--- src/todos/uses-cases/create-todo-html.js
+++ src/todos/uses-cases/create-todo-html.js
@@ -5,12 +5,21 @@ import { Todo } from "../models/todo.model";
  * @param {Todo} todo
  */
 export const createTodoHtml = (todo) => {
+  const { done, description, id } = todo;
   if (!todo) throw new Error("A TODO object is required");
 
-  const html = `<h1> ${todo.description}</h1>`;
-
+  const html = `
+  <div class="view">
+      <input class="toggle" type="checkbox" ${done ? "checked" : ""}>
+      <label>${description}</label>
+      <button class="destroy"></button>
+  </div>
+  <input class="edit" value="Create a TodoMVC template">
+`;
   const liEelement = document.createElement("li");
   liEelement.innerHTML = html;
+  liEelement.setAttribute("data-id", id);
+  done && liEelement.classList.add("completed");
 
   return liEelement;
 };
